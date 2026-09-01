4288727c3f156b7e31c2dd3a1344a6c4e9e4aa62
Nishan <nishan9207@gmail.com>
2026-09-02 02:37:36 +0530

    feat: add dynamic category dropdown and combined filtering logic

--- script.js
+++ script.js
@@ -21,16 +21,17 @@ transactionForm.addEventListener('submit', function(event){
     transactions.push(newTransaction);
 
     console.log('Current Transaction Array:', transactions);
-    renderTransactions()
+    renderTransactions(transactions)
+    updateCategoryDropDown();
     transactionForm.reset();
 });
 
 const transactionList = document.getElementById("transaction-list");
 
-function renderTransactions() {
+function renderTransactions(listToRender) {
     transactionList.innerHTML = '';
 
-    transactions.forEach(function(transaction) {
+    listToRender.forEach(function(transaction) {
         const li = document.createElement('li');
 
         const sign = transaction.type === 'income' ? '+' : '-';
@@ -48,13 +49,13 @@ function renderTransactions() {
             deleteTransaction(transaction.id);
         })
 
-        amountSpan.textContent = `${sign}₹${Math.abs(transaction.amount).toFixed(3)}`
-        typeSpan.Textcontent = `${transaction.type}`
+        amountSpan.textContent = `${sign}₹${Math.abs(transaction.amount).toFixed(3)}`;
+        typeSpan.textContent = `${transaction.type}`;
         descSpan.textContent = `${transaction.description} (${transaction.category})`;
 
-        li.appendChild(descSpan);
         li.appendChild(amountSpan);
         li.appendChild(typeSpan)
+        li.appendChild(descSpan);        
         li.appendChild(deleteBtn);
 
         transactionList.appendChild(li);
@@ -65,6 +66,43 @@ function deleteTransaction(id) {
     transactions = transactions.filter(function(transaction){
         return transaction.id != id
     })
-    renderTransactions();
+
+    updateCategoryDropDown();
+    renderTransactions(transactions);
 }
 
+
+const categoryFilter = document.getElementById("category-filter")
+
+function updateCategoryDropDown() {
+    const categories = transactions.map(function(transaction){
+        return transaction.category;
+    });
+
+    uniqueCategories = [...new Set(categories)];
+    categoryFilter.innerHTML = `<option value="all">All categories</option>` ;
+
+    uniqueCategories.forEach(function(category) {
+        const option = document.createElement('option');
+        option.value = category;
+        option.textContent = category;
+        categoryFilter.appendChild(option);
+    });
+}
+
+const typeFilter = document.getElementById("type-filter");
+
+typeFilter.addEventListener('change', filterTransactions);
+categoryFilter.addEventListener('change', filterTransactions);
+
+function filterTransactions(){
+    
+    selectedType = typeFilter.value;
+    selectedCategory = categoryFilter.value;
+    const filtered = transactions.filter(function(transaction) {
+        const matchesType = (selectedType === 'all') || (transaction.type === selectedType);
+        const matchesCategory = (selectedCategory === 'all') || (transaction.category === selectedCategory);
+         return matchesType && matchesCategory
+    })
+    renderTransactions(filtered)
+}
