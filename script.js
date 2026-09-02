380e5c8c36e9a6a4b755eb28f85740e14086d2a9
Nishan <nishan9207@gmail.com>
2026-09-02 17:05:16 +0530

    feat: implement form editing, event delegation on parent container

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const storedTransactions = localStorage.getItem('transactions');
 let transactions = storedTransactions ? JSON.parse(storedTransactions) : [];
 
 const transactionForm = document.getElementById("transaction-form");
+const saveBtn = document.getElementById('submit-btn');
+let editingId = null;
+
 const amountInput = document.getElementById("amount");
 const typeInput = document.getElementById("transaction-type");
 const categoryInput = document.getElementById("category");
@@ -16,6 +19,7 @@ const currentBalance = document.getElementById("current-balance");
 const totalIncome = document.getElementById("total-income");
 const totalExpense = document.getElementById("total-expense")
 
+
 renderTransactions(transactions);
 updateCategoryDropDown();
 updateSummary();
@@ -28,8 +32,7 @@ function saveTransactions(){
 
 transactionForm.addEventListener('submit', addTransaction);
 
-function addTransaction(event) {
-    event.preventDefault();
+function validateForm() {
 
     clearErrors();
 
@@ -39,50 +42,40 @@ function addTransaction(event) {
     const typeValue = typeInput.value;
     const descriptionValue = descriptionInput.value.trim();
 
-    let hasError = false
+    let isValid = true
 
     if (isNaN(amountValue) || amountValue <=0) {
         showError('amount-error', 'Please enter a valid amount greater than 0.');
-        hasError = true;
+        isValid = false;
     }
 
     const allowedTypes = ['income', 'expense'];
     if (!allowedTypes.includes(typeValue)) {
         showError('type-error', 'Please select a valid type (Income or Expense).');
-        hasError = true;
+        isValid = false;
     }
 
     if (categoryValue === '') {
         showError('category-error', 'Please enter or select a category.');
-        hasError = true
+        isValid = false
     }
 
     if (dateValue === '' || isNaN(Date.parse(dateValue))) {
         showError('date-error', 'Please select a valid date.');
-        hasError = true;
+        isValid = false;
     }
 
-    if (hasError) return ;
+    if (!isValid) return null;
 
-    const newTransaction = {
-        id: Date.now(),
+    return {
         amount: amountValue,
         type: typeInput.value,
         category: categoryValue,
         date: dateValue,
         description: descriptionValue
     };
-
-    transactions.push(newTransaction);
-    saveTransactions();
-
-    console.log('Current Transaction Array:', transactions);
-    renderTransactions(transactions)
-    updateCategoryDropDown();
-    updateSummary()
-
-    transactionForm.reset();
 }
+
 
 function showError(message) {
     const errorElement = document.getElementById(elementId);
@@ -94,6 +87,46 @@ function clearErrors() {
     errorElements.forEach(function(el){
         el.textContent ='';
     })
+}
+
+function addTransaction(event) {
+    event.preventDefault();
+
+    const formData = validateForm();
+    if (!formData) return;
+
+    if (editingId === null) {
+
+        const newTransaction = {
+            id: Date.now(),
+            ...formData
+        };
+        transactions.unshift(newTransaction);
+    }
+    else {
+        const index = transactions.findIndex(function (transaction) {
+            return transaction.id = editingId;
+        });
+
+        if (index!== -1) {
+            transactions[index] = {
+                id: editingId,
+                ...formData
+            };
+        }
+
+        editingId === null;
+        saveBtn.textContent = 'Save';
+    }
+
+    saveTransactions();
+
+    console.log('Current Transaction Array:', transactions);
+    renderTransactions(transactions)
+    updateCategoryDropDown();
+    updateSummary()
+
+    transactionForm.reset();
 }
 
 
@@ -112,11 +145,15 @@ function renderTransactions(listToRender) {
         const amountSpan = document.createElement('span');
         const typeSpan = document.createElement('span')
         const deleteBtn = document.createElement('button');
+        const editBtn = document.createElement('button');
 
         deleteBtn.textContent = 'X';
-        deleteBtn.addEventListener('click', function(){
-            deleteTransaction(transaction.id);
-        })
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.dataset.id = transaction.id;
+
+        editBtn.textContent = 'edit';
+        editBtn.classList.add('edit-btn');
+        editBtn.dataset.id = transaction.id;
 
         amountSpan.textContent = `${sign}₹${Math.abs(transaction.amount).toFixed(2)}`;
         typeSpan.textContent = `${transaction.type}`;
@@ -126,6 +163,7 @@ function renderTransactions(listToRender) {
         li.appendChild(typeSpan)
         li.appendChild(descSpan);        
         li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
 
         transactionList.appendChild(li);
     });
@@ -142,6 +180,38 @@ function deleteTransaction(id) {
     updateSummary();
 
 }
+
+function startEdit(id) {
+    const transactionToEdit = transactions.find(function(transaction) {
+        return transaction.id === id;
+    });
+
+    if (!transactionToEdit) return;
+
+    amountInput.value = transactionToEdit.amount;
+    typeInput.value = transactionToEdit.type;
+    categoryInput.value = transactionToEdit.category
+    dateInput.value = transactionToEdit.date;
+    descriptionInput.value = transactionToEdit.description
+
+    editingId = id;
+    saveBtn.textContent = 'Update';
+}
+
+// Event listner for edit and delete button, delegation to parent container
+
+transactionList.addEventListener('click', function(e){
+    if (e.target.classList.contains('delete-btn')) {
+        const clickedId = Number(e.target.dataset.id);
+        deleteTransaction(clickedId);
+    }
+
+    if (e.target.classList.contains('edit-btn')) {
+        const clickedId = Number(e.target.dataset.id);
+        startEdit(clickedId);
+        return;
+    }
+});
 
 
 function updateCategoryDropDown() {
